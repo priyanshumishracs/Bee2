04d2a2ace4f84e5c9a139395e7b209d6d6bbd3f4
priyanshumishracs <priyanshu.sirte@gmail.com>
2025-09-03 15:32:31 +0530

    inital commit stage 5

--- main.tf
+++ main.tf
@@ -15,21 +15,21 @@ resource "azurerm_virtual_network" "Vnet" {
 
 # Vnet1-Subnet1
 resource "azurerm_subnet" "subnet1" {
-  name                 = var.Vnet_subnet1_name
+  name                 = "${terraform.workspace}-${var.Vnet_subnet1_name}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.Vnet.name
   address_prefixes     = [var.Vnet_subnet1_address_prefix]
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = var.Vnet_subnet2_name
+  name                 = "${terraform.workspace}-${var.Vnet_subnet2_name}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.Vnet.name
   address_prefixes     = [var.Vnet_subnet2_address_prefix]
 }
 
 resource "azurerm_network_security_group" "app" {
- name = "${terraform.workspace}_${length(var.Vm_names)}-nsg1"
+ name = "${terraform.workspace}_${length(var.Vm_names)}-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
